5d3cdbca26bb54224a63a9fd892831cafe9e5721
jltran <guardian@berkeley.edu>
2014-12-03 17:51:08 -0800

    set-up heroku mongolab db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
-    url: 'mongodb://localhost/test'
+    //url: 'mongodb://localhost/test'
+    url: 'mongodb://guardian:physicals44@ds053380.mongolab.com:53380/heroku_app32192480'
 };
