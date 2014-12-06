2bce620aab74aff96fb18cefd32bfd49962446e1
jltran <guardian@berkeley.edu>
2014-12-06 00:16:05 -0800

    db pathways

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 module.exports = {
     //url: 'mongodb://localhost/test'
-    url: 'mongodb://pharmacy:,anip..b@ds053380.mongolab.com:53380/heroku_app32192480'
+    url: 'mongodb://kodan:duplexdna@dogen.mongohq.com:10048/professor_x'
 };
